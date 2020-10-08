95367fee3b9b62cd1593b078da5d0176099887a5
ferry-fauzan <fauzanferry123@gmail.com>
2020-10-08 16:49:01 +0700

    Revisi source code js

--- JamDigital/jamDigital.js
+++ JamDigital/jamDigital.js
@@ -7,13 +7,3 @@ function waktu() {
     document.getElementById("menit").innerHTML = waktu.getMinutes();
     document.getElementById("detik").innerHTML = waktu.getSeconds();
 }
-
-// window.setTimeout("waktu()", 1000);
-
-// function waktu() {
-//     var waktu = new Date();
-//     setTimeout("waktu()", 1000);
-//     document.getElementById("jam").innerHTML = waktu.getHours();
-//     document.getElementById("menit").innerHTML = waktu.getMinutes();
-//     document.getElementById("detik").innerHTML = waktu.getSeconds();
-// }
