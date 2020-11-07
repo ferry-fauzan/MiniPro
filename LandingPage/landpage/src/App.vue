12f1b739b5c3cf0a753650daf2eb7ced5f02ce5d
ferry-fauzan <fauzanferry123@gmail.com>
2020-11-07 20:46:52 +0700

    Update project

---
+++ LandingPage/landpage/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-toolbar
+    color="#FFA726"
+    
+  >
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <!-- <v-divider
+      class="mx-15"
+      vertical
+    ></v-divider> -->
+
+    <span class="subheading">My Home</span>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text>
+        Home
+      </v-btn>
+
+      <!-- <v-divider vertical></v-divider> -->
+
+      <v-btn text>
+        Product
+      </v-btn>
+
+      <!-- <v-divider vertical></v-divider> -->
+
+      <v-btn text>
+        Contact
+      </v-btn>
+
+      
+      <v-btn text>
+        Contact
+      </v-btn>
+
+      <!-- <v-divider vertical></v-divider> -->
+    </v-toolbar-items>
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </v-toolbar>
+  <v-main>
+    <img src="assets/logo.png" alt="" >
+
+  </v-main>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
